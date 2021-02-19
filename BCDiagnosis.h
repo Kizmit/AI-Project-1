b02d837507a3bd3669fb3fa5597ccbf0540d7685
Joe <56753837+Kizmit@users.noreply.github.com>
2021-02-18 22:57:11 -0600

    Forgot the header

---
+++ BCDiagnosis.h
@@ -0,0 +1,9 @@
+﻿// AI XS Project 1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+// TODO: Reference additional headers your program requires here.
